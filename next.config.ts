f376eb61be08903039725e382f78b317307017bb
Nurnoah <weissenbachnoah@gmail.com>
2026-08-04 15:28:04 +0200

    Update flag quiz gameplay and interface

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: process.cwd(),
+  },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
